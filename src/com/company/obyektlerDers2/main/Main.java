2584e8863a2877212850cd0714c3fa7fb1fdd9c6
Malahat Shukurova <malakhat.shukurova@mail.ru>
2022-04-07 17:24:24 +0400

    Obyektler

---
+++ src/com/company/obyektlerDers2/main/Main.java
@@ -0,0 +1,38 @@
+package com.company.obyektlerDers2.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User.companyName = "BSP Tech";
+
+        System.out.println("company "+User.companyName);
+
+        User u = null;
+        u.name = "Mal";
+        u.companyName = "Hdgs";
+
+
+        u.name = "Sarkhan";
+        User.companyName ="BSP TECH 2";
+//
+//        System.out.println("company "+ User.companyName);
+//        System.out.println("u.name "+ u.name);
+//
+//        User u2 = new User();
+//        u2.name = "Malahat";
+//        u2.companyName = "BSP TECH 3";
+//
+//        System.out.println("company "+ User.companyName);
+//        System.out.println("u.name "+ u2.name);
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/com/company/obyektlerDers2/main/User.java
@@ -0,0 +1,16 @@
+package com.company.obyektlerDers2.main;
+
+public class User {
+    public String name;
+    public String surname;
+    public int age;
+    public static String companyName;
+
+    public static void foo(){
+
+    }
+    public void foo2(){
+    User.companyName = "TEST";
+    }
+
+}
